75edf8d712332f2b34cff71045a40a4d12bf7ec0
kelffren <kelffrencc@gmail.com>
2022-01-23 16:22:34 +0100

    h

--- js/main.js
+++ js/main.js
@@ -15,3 +15,22 @@ $(document).ready(function () {
     setTimeout(showPopup, 5500);
 
 });
+
+
+$(document).ready(function () {
+
+    function showPopup(){
+        $('.pop-up').addClass('show');
+        $('.pop-up-wrap').addClass('show');
+    }
+
+    $("#clos").click(function(){
+        $('.pop-up').removeClass('show');
+        $('.pop-up-wrap').removeClass('show');
+    });
+
+    $(".btn-abrir").click(showPopup);
+
+    setTimeout(showPopup, 5500);
+
+});
